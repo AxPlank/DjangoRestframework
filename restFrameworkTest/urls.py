8d162266aec8eb8611d0c218dae6255979dab8c6
Papus <foal0912@gmail.com>
2022-08-04 20:06:29 +0900

    Viewset as_vie() 수정

--- restFrameworkTest/urls.py
+++ restFrameworkTest/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 # views.py
-# from restFrameworkTest.views.views_APIViews import *
-# from restFrameworkTest.views.views_Mixins import *
-# from restFrameworkTest.views.views_Generic import *
-from restFrameworkTest.views.views_Viewset import *
+from restFrameworkTest.views.views_APIViews import FootballPlayerList, FootballPlayerDetail
+from restFrameworkTest.views.views_Mixins import FootballPlayerList, FootballPlayerDetail
+from restFrameworkTest.views.views_Generic import FootballPlayerList, FootballPlayerDetail
+from restFrameworkTest.views.views_Viewset import FootballPlayerViewset
 # Viewset
 from rest_framework.routers import DefaultRouter
 
 """
-as_view()
+as_view(), Written using Viewset
 """
 footballplayer_list = FootballPlayerViewset.as_view(
     {'get': 'list',
@@ -18,24 +18,25 @@ footballplayer_list = FootballPlayerViewset.as_view(
 footballpalyer_detail = FootballPlayerViewset.as_view(
     {'get': 'retrieve',
      'put': 'update',
+     'patch': 'partial_update',
      'delete': 'destroy'}
 )
 
 """
 router
 """
-# r = DefaultRouter()
-# r.register('', FootballPlayerViewset)
+r = DefaultRouter()
+r.register('', FootballPlayerViewset)
 
 urlpatterns = [
-    # # APIView, Mixins, Generic
-    # path('', FootballPlayerList.as_view(), name='players_list'),
-    # path('<int:pk>', FootballPlayerDetail.as_view(), name='player'),
-    #
-    # # Viewset - as_view()
+    # APIView, Mixins, Generic
+    path('', FootballPlayerList.as_view(), name='players_list'),
+    path('<int:pk>', FootballPlayerDetail.as_view(), name='player'),
+
+    # Viewset - as_view()
     path('', footballplayer_list),
     path('<int:pk>/', footballpalyer_detail),
 
-    # # viewset - router
-    # path('', include(r.urls)),
+    # viewset - router
+    path('', include(r.urls)),
 ]
