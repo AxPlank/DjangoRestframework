8543ada1c6713c4274dfde110ce2046b9815db7a
Papus <foal0912@gmail.com>
2022-08-15 01:10:40 +0900

    React 설정

--- football-player/src/footballplayer.js
+++ football-player/src/footballplayer.js
@@ -1,1 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
+
+function App() {
+    return (
+        <section>
+            <div>
+                Name: <input type="text"></input>
+                Team: <input type="text"></input>
+                Position: <input type="text"></input>
+                Uniform Number: <input type="text"></input>
+                <button>
+                    선수 등록
+                </button>
+            </div>
+        </section>
+    );
+}
+
+function getData() {
+
+}
+
+function postData() {
+
+}
+
+function putpatchData() {
+
+}
+
+function deleteData() {
+
+}
+
+export default App;
